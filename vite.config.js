055cfc724cd61334df81ec0c77f02749d4d2d51b
thippeshr6599 <thippeshrclk87@gmail.com>
2026-07-23 13:21:57 +0530

    Fix GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 
 export default defineConfig({
-  base: '/Internms/', // Add this line
+  base: '/Internms/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
