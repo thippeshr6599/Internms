191a748c9c7f71e22759e895b56038afa6b10d04
thippeshr6599 <thippeshrclk87@gmail.com>
2026-07-25 11:01:23 +0530

    Updated Intern dashboard image Css

--- src/Interndashboard/Internsidebar.jsx
+++ src/Interndashboard/Internsidebar.jsx
@@ -28,32 +28,32 @@ export default function Internsidebar() {
           </div>
           <div className='Header-sidbar-IDB'>
             <div className='header-sidebar-IDB'>
-                <img src={Myinternship} alt="internshipimage" />
+            <img src={Myinternship} alt="internshipimage" style={{width:"21px", height:"21px",marginLeft:"20px"}} />
                 <span>My Internships</span>
             </div>
             <div className='header-sidebar-IDB'>
-                <img src={Applicationstatus} alt="internshipimage" />
+                <img src={Applicationstatus} alt="Applicationstatus" style={{width:"16.9px", height:"21.54px",marginLeft:"20px"}} />
                 <span>Application Status</span>
             </div><div className='header-sidebar-IDB'>
-                <img src={Assignedmentor} alt="internshipimage" />
+                <img src={Assignedmentor} alt="Assignedmentor" style={{width:"18px", height:"18px",marginLeft:"20px"}} />
                 <span>Assigned Mentor</span>
             </div><div className='header-sidebar-IDB'>
-                <img src={Progresstracking} alt="internshipimage" />
+                <img src={Progresstracking} alt="Progresstracking" style={{width:"24px", height:"24px",marginLeft:"15px"}}/>
                 <span>Progress Tracking</span>
             </div><div className='header-sidebar-IDB'>
-                <img src={Weeklyreports} alt="internshipimage" />
+                <img src={Weeklyreports} alt="Weeklyreports" style={{width:"17.52px", height:"21.60px",marginLeft:"20px"}}/>
                 <span>Weekly Reports</span>
             </div><div className='header-sidebar-IDB'>
-                <img src={TasksIDB} alt="internshipimage" />
+                <img src={TasksIDB} alt="TaskIDB" style={{width:"24px", height:"24px",marginLeft:"15px"}} />
                 <span>Tasks & Deliverables</span>
             </div><div className='header-sidebar-IDB'>
-                <img src={Notificationsidb} alt="internshipimage" />
+                <img src={Notificationsidb} alt="Notification" style={{width:"16px", height:"20px",marginLeft:"20px"}} />
                 <span>Notifications</span>
             </div><div className='header-sidebar-IDB'>
-                <img src={Certificateidb} alt="internshipimage" />
+                <img src={Certificateidb} alt="Certificate" style={{width:"24px", height:"24px",marginLeft:"15px"}} />
                 <span>Certificates</span>
             </div><div className='header-sidebar-IDB'>
-                <img src={Performanceidb} alt="internshipimage" />
+                <img src={Performanceidb} alt="Performance" style={{width:"24px", height:"24px",marginLeft:"15px"}}/>
                 <span>Performance Metrics</span>
             </div>
           </div>
